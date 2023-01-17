4ffd85b81b29bfee711459b7d15d1e6659e54cd9
Fang-Telekom <zhi-yang.fang@telekom.de>
2023-01-17 14:51:10 +0100

    FOM

---
+++ c/8_Strings/4_Extraction.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+void main(){
+	char string[] = "C:/Eigene_Dateien/FOM/C-Code/main.c";
+	
+	printf("\n%s", strrchr(string, '.')+1);
+	char* name = strrchr (string, '/')+1;
+	printf("\n%s", name);
+	
+	int size = sizeof(string)/sizeof(char);
+	int nSize = sizeof(name)/sizeof(char);
+	string[size-nSize+1] = '\0';
+	printf("\n%s\n", string);
+
+}
+
