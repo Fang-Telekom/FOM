fe6fc05999bbc3ad691086447b6ee83512212917
Fang-Telekom <zhi-yang.fang@telekom.de>
2023-01-22 13:00:52 +0100

    FOM

---
+++ c/4_Funktionen/3_Kreis/kreis.c
@@ -0,0 +1,12 @@
+#include "funktion.h"
+
+void main(){
+	float r, pi = 3.14159265359;
+	printf("\n\nWillkommen zum Kreisrechner!\n");
+	printf("Geben Sie bitte den Radius des Kreises ein: ");
+	scanf("%f",&r);
+
+	printf("\nDer Kreisumfang ist \t %.02f", umfang(r, pi));
+	printf("\nDer Flächeninhalt ist \t %.02f", inhalt(r, pi));
+	printf("\nVielen Dank für Ihren Beitrag!\n\n");
+}
