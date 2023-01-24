941f9b84e6fd328d7af1caa5df21573efaaf38e1
Fang-Telekom <zhi-yang.fang@telekom.de>
2023-01-24 08:44:42 +0100

    FOM

--- c/8_Strings/3_Names/name.c
+++ c/8_Strings/3_Names/name.c
@@ -1,20 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-char ausgabe[](char* vorname, char* nachname){
-	char zeile[81];
-	strcpy(zeile, vorname);
-	for(int x = strlen(vorname); x <= 80 - strlen(nachname); x++){
-		zeile[x] = 'x';
-	}
-	strcat(zeile, nachname);
-	zeile[81] = '\0';
-	return zeile[0];
-}
-
 void main(){
 	char* vorname[5] = {"Franz", "Meriam", "Justin", "Kai", "Bob"};
-	char* nachname[5] = {"Bauer", "Schmidt", "Müller", "Fang", "Baumeister"};
+	char* nachname[5] = {"Bauer", "Schmidt", "Mueller", "Fang", "Baumeister"};
+        char* underscore = "_";
 
 	printf("\n\nWillkommen. Hier werden 5 Namen aufgelistet.\n");
 	
@@ -22,11 +12,10 @@ void main(){
 		char zeile[81];
 		strcpy(zeile, vorname[i]);
 		for(int x = strlen(vorname[i]); x <= 80 - strlen(nachname[i]); x++){
-			zeile[x] = 'x';
+		 	strcat(zeile, underscore);
 		}
 		strcat(zeile, nachname[i]);
-		zeile[81] = '\0';
-		
+		// zeile[81] = '\0';
 		printf("\n%s",zeile);
 	}
 	printf("\n\n");
