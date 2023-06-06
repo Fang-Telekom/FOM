f7c0a20a19f2bbde40d3c2d45486d7598a961bc9
Fang-Telekom <zhi-yang.fang@telekom.de>
2023-06-06 10:53:10 +0000

    FOM

--- java/1_Verwaltung/Verwaltung.java
+++ java/1_Verwaltung/Verwaltung.java
@@ -5,6 +5,6 @@ public class Verwaltung(){
 
 	}
 	public static void main(String[] args){
-
+		private Stack<Buch> buch;
 	}
 }
--- java/1_Verwaltung/medium/Stack.java
+++ java/1_Verwaltung/medium/Stack.java
@@ -3,12 +3,15 @@ package medium;
 public class Stack<M extends medium>{
 	
 	private Object stack[];
-
-	public Stack(nt size){
-		stack = new Object[size];
+	
+	public Stack(){
+		stack = null;
+	}
+	public Stack(M... medium){
+		stack = medium;
 	}
 
-	public void get(int i){
+	public M get(int i){
 		if(i<0)
 			return stack[0];
 		else if(i>stack.length)
@@ -16,11 +19,17 @@ public class Stack<M extends medium>{
 		return stack[i];
 	}
 	public void push(M ob){
-		Object temp[] = new Object [stack.length + 1];
-		for(int i = 0; i < stack.length; i++)
-			temp[i] = stack[i];
-		temp=[stack.length]=ob;
-		stack = temp;
+		if(stack != null){
+			Object temp[] = M [stack.length + 1];
+			for(int i = 0; i < stack.length; i++)
+				temp[i] = stack[i];
+			temp=[stack.length]=ob;
+			stack = temp;
+		}
+		else{
+			stack = M [1];
+			stack[0] = ob;
+		}
 	}
 	public int size(){
 		return stack.length;
