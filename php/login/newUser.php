8c312abaea43f3302535f7003f34aab8a7da6ea6
Fang-Telekom <zhi-yang.fang@telekom.de>
2023-11-08 19:27:58 +0100

    login

--- php/login/newUser.php
+++ php/login/newUser.php
@@ -1,35 +1,28 @@
 <?php
-	if(isset($_POST['name']) && isset($_POST['pass']) && isset($_POST['kapital']) && isset($_POST['mail'])){
-		$con = mysqli_connect("", "fom", "unifom");
+	if(isset($_POST['name']) && isset($_POST['pass']) &&
+		isset($_POST['kapital']) && isset($_POST['mail'])){
+		//Verbindung zum MySQLDatenbankserver aufnehmen
+		$con = mysqli_connect("", "fom", "unifom"); //auch möflich: Host, Benutzer, Kennwort, DB
+		
+		//Datenbank auswählen
 		mysqli_select_db($con, "uni");
-		$sql ="insert into user";
-		$sql .="(name, pass, kapital, mail) values("
-			. "'" . $_POST['name'] . "', "
-			. "'" . md5($_POST['pass']) . "', "
-			. $_POST['kapital'] . ", "
-			. "'" . $_POST['mail'] . "')";
-
-		if(mysqli_query($con, $sql)===true){
-			sleep(4);
-			?> <h1> Sie haben einen neuen Nutzer erschaffen! </h1>
-			<p> Bitte melden Sie sich an. Sie werden zurück geleitet </p>
-
-			<?php
-			sleep(4);
-			mysqli_close($con);
-			header("Location: start.php");
-			exit;
-		}else{
-			echo "<h1> Es gabe einen Fehler! </h1>";
-			echo "<p> Sie werden zurückgeleitet, versuchen Sie es erneut!";
-			sleep(4);
+		//SQL-Abfrage ausführen
+		if(mysqli_query($con, "insert into user
+			(name, pass, kapital, mail) values(
+			'" . $_POST['name'] . "',
+			'" . md5($_POST['pass']) . "', 
+			'" . $_POST['kapital'] . "', 
+			'" . $_POST['mail'] . "'")){
+				mysqli_close($con);
+				header("Location: login.php");
+				exit;
+		} else{
 			mysqli_close($con);
 			header("Location: register.php?error=1");
 			exit;
 		}
-	}else{
-		header("Location: register.php?error=1");
-		exit;
+		
+		// Verbindung schließen
+		
 	}
-	
 ?>
---
+++ php/login/tempNewUser.php
@@ -0,0 +1,36 @@
+<?php
+	if(isset($_POST['name']) && isset($_POST['pass'])
+		&& isset($_POST['kapital']) && isset($_POST['mail'])){
+		$con = mysqli_connect("", "fom", "unifom");
+		mysqli_select_db($con, "uni");
+		$sql ="insert into user";
+		$sql .="(name, pass, kapital, mail) values("
+			. "'" . $_POST['name'] . "', "
+			. "'" . md5($_POST['pass']) . "', "
+			. $_POST['kapital'] . ", "
+			. "'" . $_POST['mail'] . "')";
+
+		if(mysqli_query($con, $sql)===true){
+			sleep(4);
+			?> <h1> Sie haben einen neuen Nutzer erschaffen! </h1>
+			<p> Bitte melden Sie sich an. Sie werden zurück geleitet </p>
+
+			<?php
+			sleep(4);
+			mysqli_close($con);
+			header("Location: start.php");
+			exit;
+		}else{
+			echo "<h1> Es gabe einen Fehler! </h1>";
+			echo "<p> Sie werden zurückgeleitet, versuchen Sie es erneut!";
+			sleep(4);
+			mysqli_close($con);
+			header("Location: register.php?error=1");
+			exit;
+		}
+	}else{
+		header("Location: register.php?error=1");
+		exit;
+	}
+	
+?>
