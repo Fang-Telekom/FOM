f4b54f0e7d2c8515e415755228bc4865550e5a83
Fang-Telekom <zhi-yang.fang@telekom.de>
2023-11-08 19:33:39 +0100

    login

--- php/login/newUser.php
+++ php/login/newUser.php
@@ -3,7 +3,7 @@
 		isset($_POST['kapital']) && isset($_POST['mail'])){
 		//Verbindung zum MySQLDatenbankserver aufnehmen
 		$con = mysqli_connect("", "fom", "unifom"); //auch möflich: Host, Benutzer, Kennwort, DB
-		
+		echo $con;
 		//Datenbank auswählen
 		mysqli_select_db($con, "uni");
 		//SQL-Abfrage ausführen
@@ -14,15 +14,15 @@
 			'" . $_POST['kapital'] . "', 
 			'" . $_POST['mail'] . "'")){
 				mysqli_close($con);
+				echo "HI";
 				header("Location: login.php");
 				exit;
 		} else{
+			echo "HIll";
 			mysqli_close($con);
 			header("Location: register.php?error=1");
 			exit;
 		}
-		
-		// Verbindung schließen
 		
 	}
 ?>
