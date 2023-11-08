53a740453be3171d4037c3c502a541518d35a24b
Fang-Telekom <zhi-yang.fang@telekom.de>
2023-11-08 18:24:22 +0100

    login

--- php/login/login.php
+++ php/login/login.php
@@ -1,31 +1,30 @@
 <?php
 	session_start();
-	
+
 	if(isset($_POST['name']) && isset($_POST['pass'])){
-		$con = mysqli_connect("", "fom", "unifom");
-		mysqli_select_db($con, "uni");
-	
-		$sql = "select * from user where"
-			. " name='" . $_POST['name'] . "' and"
-			. " pass='" . md5($_POST['pass']) . "'";
-
-		$req = mysqli_query($con, $sql);
-		$num = mysqli_num_rows($res);
+		//Verbindung zum MySQLDatenbankserver aufnehmen
+		$con = mysqli_connect("", "fom", "unifom"); //auch möflich: Host, Benutzer, Kennwort, DB
 		
-		mysqli_close($con);
+		//Datenbank auswählen
+		mysqli_select_db($con, "uni");
+		//SQL-Abfrage ausführen
+		$req=mysqli_query($con, "select * from user where name='" . $_POST['name'] .
+		"' and pass='" . md5($_POST['pass']) . "'");
+		//Anzahl Datensätze ermitelln und ausgeben
+		$num = mysqli_num_rows($req);
 
+		echo "$num Datensätze wurde gefunden";
 		if($num >= 1){
-			$_SESSION['name']=$_POST['name'];
-			$_SESSION['login']="1";
-			header("Location: willkommen.php"); //Header für die Umleitung
+			$_SESSION['name'] = $_POST['name'];
+			$_SESSION['login'] = "1";
+			header("Location: willkommen.php");
 			exit;
-		}
+		} else
+			mysqli_close($con);
 	} else{
-		header("Location:start.php?error=1"); //Setzung des Errors
+		header("Location: start.php?error=1");
 		exit;
-	
 	}
-
 ?>
 
-	
+
