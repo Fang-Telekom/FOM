50e8c035660b7338933b5a550ff066577f227de2
Fang-Telekom <zhi-yang.fang@telekom.de>
2023-11-14 23:59:53 +0100

    willkommen

--- php/login/willkommen.php
+++ php/login/willkommen.php
@@ -12,6 +12,46 @@
 
 	<meta charset="UTF-8" />
 	<title> Willkommen im geschützten Bereich </title>
+
+	<style>
+		.sticky-header {
+        top: 0;
+        left: 0;
+        width: 100%;
+        min-height: 6rem;
+        height: auto;
+        
+        z-index: 100; /*Es über andere Inhalte liegen*/
+        margin: 0 auto;
+        padding: 0;
+        text-align: center;
+        position: sticky;
+
+        background-color: black;
+        color: white;
+        text-align: center;
+        line-height: 1;
+    	}
+		nav{
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        width: 100%;
+        height: 40px;
+        background-color: black;
+		}
+		a{
+			display: inline-block;
+			padding: 10px 15px;
+			line-height: 1;
+			color: white;
+			text-decoration: none;
+			transition: 0.5s ease-in;
+		}
+		a:hover{
+			border-bottom: 1px solid white;
+		}
+	</style>
 </head>
 
 <body>
