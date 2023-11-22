5b48bedb3b5752238fb4070ba5915f997e862698
Fang-Telekom <zhi-yang.fang@telekom.de>
2023-11-22 21:56:20 +0100

    add request function

--- php/login/requestCredit.php
+++ php/login/requestCredit.php
@@ -2,7 +2,7 @@
 	session_start();
 	$code="6";
 	
-	if(isset($_POST['credit'])){
+	if(isset($_POST['credit']) && is_array($_POST['credit'])){
 		$code="7";
 		//Verbindung zum MySQLDatenbankserver aufnehmen
 		$con = mysqli_connect("", "fom", "unifom"); //auch möflich: Host, Benutzer, Kennwort, DB
@@ -10,6 +10,7 @@
 		//Datenbank auswählen
 		mysqli_select_db($con, "uni");
 		//SQL-Abfrage ausführen
+
 		foreach($_POST['credit'] as $c){
 			mysqli_query($con, "insert into request(kredit, requester, granted) value('"
 			. $c . "', '" . $_SESSION['id'] . "', 'pending')");
