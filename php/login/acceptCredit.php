a142524c29a0afa607c297a8540e142d9e943564
Fang-Telekom <zhi-yang.fang@telekom.de>
2023-11-22 22:34:39 +0100

    correct sql error

--- php/login/acceptCredit.php
+++ php/login/acceptCredit.php
@@ -14,7 +14,7 @@
 			$code="9";
 		if(mysqli_query($con, "update request set granted='granted' where requester={$_POST['accept']} and kredit={$_POST['kredit']}"))
 			$code="10";
-		if(mysqli_query($con, "update kredit set requester={$_POST['accept']}, granted='" . date("Y-m-d") . "' where kredit={$_POST['kredit']}"))
+		if(mysqli_query($con, "update kredit set requester={$_POST['accept']}, granted='" . date("Y-m-d") . "' where id={$_POST['kredit']}"))
 			$code="11";
 		// Verbindung schließen
 		mysqli_close($con);
