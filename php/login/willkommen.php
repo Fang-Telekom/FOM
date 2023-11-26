380dde891182ada3110e84dcf23810bb9f639380
Fang-Telekom <zhi-yang.fang@telekom.de>
2023-11-26 16:51:41 +0100

    adjust visual aspect

--- php/login/willkommen.php
+++ php/login/willkommen.php
@@ -43,98 +43,102 @@
 
 <body>
 	<div class="body">
-		<div class="row">
 <?php
 		echo "<h1>Willkommen {$_SESSION['name']} </h1>";
 		echo "<p> Kapital: " . $_SESSION['kapital'] . "</p>";
 		echo "<p> ID: " . $_SESSION['id'] . "</p>";
 ?>
+		
+		<div class="row">
+			<div class="w50">
+				<h3> Geld überweisen </h3>
+
+				<?php 
+				if(isset($_GET['code']) && $_GET['code'] == 0){
+					?><p class="code"> Bitte füllen Sie alles aus </p> <?php
+				} else if(isset($_GET['code']) && $_GET['code'] == 1){
+					?><p class="code"> Es besteht keine Verbindung zum Datenbank</p> <?php
+				}else if(isset($_GET['code']) && $_GET['code'] == 2){
+					?><p class="code"> Die Summe konnte nicht überwiesen werden</p> <?php
+				}else if(isset($_GET['code']) && $_GET['code'] == 3){
+					?><p class="code"> Die Transaktion war erfolgreich</p> <?php
+				}else if(isset($_GET['code']) && $_GET['code'] == -1){
+					?><p class="code"> Es kann nicht an sich überwiesen werden.</p> <?php
+				}
+				?>
+
+				<form action="senden.php" method="post">
+					<p> Konto ID </p>
+					<input type="text" id="id" name="id"> <br/>
+					<p> Inhaber Name </p>
+					<input type="text" id="name" name="name"> <br/>
+					<p> Summe </p>
+					<input type="number" id="summe" name="summe"><br/>
+					<p> Kommentar </p>
+					<input type="text" id="comment" name="comment"> <br/>
+					
+					<input type="submit" value="Senden">
+					<input type="reset" value="Zurücksetzen">
+				</form>
+			</div>
+			<div class="w50">
+				<h3> Transaktionen </h3>
+				<?php include "transaction.php"; ?>
+			</div>
 		</div>
-		<div class="row"> <div class="w50">
-		<h3> Geld überweisen </h3>
-
-		<?php 
-		if(isset($_GET['code']) && $_GET['code'] == 0){
-			?><p class="code"> Bitte füllen Sie alles aus </p> <?php
-		} else if(isset($_GET['code']) && $_GET['code'] == 1){
-			?><p class="code"> Es besteht keine Verbindung zum Datenbank</p> <?php
-		}else if(isset($_GET['code']) && $_GET['code'] == 2){
-			?><p class="code"> Die Summe konnte nicht überwiesen werden</p> <?php
-		}else if(isset($_GET['code']) && $_GET['code'] == 3){
-			?><p class="code"> Die Transaktion war erfolgreich</p> <?php
-		}else if(isset($_GET['code']) && $_GET['code'] == -1){
-			?><p class="code"> Es kann nicht an sich überwiesen werden.</p> <?php
-		}
-		?>
-
-		<form action="senden.php" method="post">
-			<p> Konto ID </p>
-			<input type="text" id="id" name="id"> <br/>
-			<p> Inhaber Name </p>
-			<input type="text" id="name" name="name"> <br/>
-			<p> Summe </p>
-			<input type="number" id="summe" name="summe"><br/>
-			<p> Kommentar </p>
-			<input type="text" id="comment" name="comment"> <br/>
-			
-			<input type="submit" value="Senden">
-			<input type="reset" value="Zurücksetzen">
-		</form>
-		</div> <div class="w50">
-		<h3> Transaktionen </h3>
-		<?php include "transaction.php"; ?>
-		</div> </div>
 		<div class="row">
-		<h3> Kredit Board </h3>
-		<div class="w50">
-		<h4> Meine Kredit </h4>
-		<?php include "myCredit.php";
-		?>
-		</div> </div>
+			<h3> Kredit Board </h3>
+			<div class="w50">
+				<h4> Meine Kredit </h4>
+				<?php include "myCredit.php";
+				?>
+			</div>
+		</div>
 		<div class="row">
-		<div class="w50"> <?php
-		if(!($_SESSION['request'])){
-			echo "<h4> Kreditausschreiben </h4>";
+			<div class="w50"> <?php
+				if(!($_SESSION['request'])){
+					echo "<h4> Kreditausschreiben </h4>";
 
-			if(isset($_GET['code']) && $_GET['code'] == 4){
-				?><p class="code"> Bitte füllen Sie alles aus </p> <?php
-			} else if(isset($_GET['code']) && $_GET['code'] == 5){
+					if(isset($_GET['code']) && $_GET['code'] == 4){
+						?><p class="code"> Bitte füllen Sie alles aus </p> <?php
+					} else if(isset($_GET['code']) && $_GET['code'] == 5){
+						?><p class="code"> Erfolgreich Ausgeschrieben</p> <?php
+					}
+					?>
+
+					<form action="postCredit.php" method="post">
+						Kredithöhe <br/>
+						<input type="number" name="credit" size="20">
+						<br/>
+						Zins (%) <br/>
+						<input type="number" name="interest" size="20">
+						<br/>
+						<input type="submit" value="Auschreiben">
+						<input type="reset" value="Reset">
+					</form>
+			</div>
+			<div class="w50">
+				<?php
+				echo "<h4> Kreditannehmen </h4>";
+				if(isset($_GET['code']) && $_GET['code'] == 8){
+					?><p class="code"> Bitte wählen Sie mindestens einen Kredit </p> <?php
+				} else if(isset($_GET['code']) && $_GET['code'] == 11){
+					?><p class="code"> Erfolgreich Angenommen</p> <?php
+				}
+				include "creditRequest.php";
+				}?>
+			</div>
+		</div>
+		<div class="row">
+				<?php
+			echo "<h4> Ausgeschrieben </h4>";
+			if(isset($_GET['code']) && $_GET['code'] == 6){
+				?><p class="code"> Bitte wählen Sie mindestens einen Kredit </p> <?php
+			} else if(isset($_GET['code']) && $_GET['code'] == 7){
 				?><p class="code"> Erfolgreich Ausgeschrieben</p> <?php
 			}
+			include "credits.php";
 			?>
-
-			<form action="postCredit.php" method="post">
-				Kredithöhe <br/>
-				<input type="number" name="credit" size="20">
-				<br/>
-				Zins (%) <br/>
-				<input type="number" name="interest" size="20">
-				<br/>
-				<input type="submit" value="Auschreiben">
-				<input type="reset" value="Reset">
-			</form>
-		</div>
-		<div class="w50">
-			<?php
-			echo "<h4> Kreditannehmen </h4>";
-			if(isset($_GET['code']) && $_GET['code'] == 8){
-				?><p class="code"> Bitte wählen Sie mindestens einen Kredit </p> <?php
-			} else if(isset($_GET['code']) && $_GET['code'] == 11){
-				?><p class="code"> Erfolgreich Angenommen</p> <?php
-			}
-			include "creditRequest.php";
-		}?>
-		</div> </div>
-		<div class="row">
-			<?php
-		echo "<h4> Ausgeschrieben </h4>";
-		if(isset($_GET['code']) && $_GET['code'] == 6){
-			?><p class="code"> Bitte wählen Sie mindestens einen Kredit </p> <?php
-		} else if(isset($_GET['code']) && $_GET['code'] == 7){
-			?><p class="code"> Erfolgreich Ausgeschrieben</p> <?php
-		}
-		include "credits.php";
-		?>
 		</div>
 	</div>
 </body>
