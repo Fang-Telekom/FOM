2373ba488d9e009cacab2341c1666dc4aafa3cfd
Fang-Telekom <zhi-yang.fang@telekom.de>
2023-11-26 17:04:19 +0100

    adjust visual aspect

--- php/login/willkommen.php
+++ php/login/willkommen.php
@@ -50,75 +50,68 @@
 ?>
 		
 		<div class="row">
-			<div class="w50">
-				<h3> Geld überweisen </h3>
+			<h3> Geld überweisen </h3>
 
-				<?php 
-				if(isset($_GET['code']) && $_GET['code'] == 0){
-					?><p class="code"> Bitte füllen Sie alles aus </p> <?php
-				} else if(isset($_GET['code']) && $_GET['code'] == 1){
-					?><p class="code"> Es besteht keine Verbindung zum Datenbank</p> <?php
-				}else if(isset($_GET['code']) && $_GET['code'] == 2){
-					?><p class="code"> Die Summe konnte nicht überwiesen werden</p> <?php
-				}else if(isset($_GET['code']) && $_GET['code'] == 3){
-					?><p class="code"> Die Transaktion war erfolgreich</p> <?php
-				}else if(isset($_GET['code']) && $_GET['code'] == -1){
-					?><p class="code"> Es kann nicht an sich überwiesen werden.</p> <?php
-				}
-				?>
+			<?php 
+			if(isset($_GET['code']) && $_GET['code'] == 0){
+				?><p class="code"> Bitte füllen Sie alles aus </p> <?php
+			} else if(isset($_GET['code']) && $_GET['code'] == 1){
+				?><p class="code"> Es besteht keine Verbindung zum Datenbank</p> <?php
+			}else if(isset($_GET['code']) && $_GET['code'] == 2){
+				?><p class="code"> Die Summe konnte nicht überwiesen werden</p> <?php
+			}else if(isset($_GET['code']) && $_GET['code'] == 3){
+				?><p class="code"> Die Transaktion war erfolgreich</p> <?php
+			}else if(isset($_GET['code']) && $_GET['code'] == -1){
+				?><p class="code"> Es kann nicht an sich überwiesen werden.</p> <?php
+			}
+			?>
 
-				<form action="senden.php" method="post">
-					<p> Konto ID </p>
-					<input type="text" id="id" name="id"> <br/>
-					<p> Inhaber Name </p>
-					<input type="text" id="name" name="name"> <br/>
-					<p> Summe </p>
-					<input type="number" id="summe" name="summe"><br/>
-					<p> Kommentar </p>
-					<input type="text" id="comment" name="comment"> <br/>
-					
-					<input type="submit" value="Senden">
-					<input type="reset" value="Zurücksetzen">
-				</form>
-			</div>
-			<div class="w50">
-				<h3> Transaktionen </h3>
-				<?php include "transaction.php"; ?>
-			</div>
+			<form action="senden.php" method="post">
+				<p> Konto ID </p>
+				<input type="text" id="id" name="id"> <br/>
+				<p> Inhaber Name </p>
+				<input type="text" id="name" name="name"> <br/>
+				<p> Summe </p>
+				<input type="number" id="summe" name="summe"><br/>
+				<p> Kommentar </p>
+				<input type="text" id="comment" name="comment"> <br/>
+				
+				<input type="submit" value="Senden">
+				<input type="reset" value="Zurücksetzen">
+			</form>
+			<h3> Transaktionen </h3>
+			<?php include "transaction.php"; ?>
 		</div>
 		
 		<h3> Kredit Board </h3>
-		<div class="row">			
-			<div class="w50">
-				<h4> Meine Kredit </h4>
-				<?php include "myCredit.php";
-				?>
-			</div>
+		<div class="row">					
+			<h4> Meine Kredit </h4>
+			<?php include "myCredit.php";
+			?>
 		</div>
 		<div class="row">
-			<div class="w50"> <?php
-				if(!($_SESSION['request'])){
-					echo "<h4> Kreditausschreiben </h4>";
+			<?php
+			if(!($_SESSION['request'])){
+				echo "<h4> Kreditausschreiben </h4>";
 
-					if(isset($_GET['code']) && $_GET['code'] == 4){
-						?><p class="code"> Bitte füllen Sie alles aus </p> <?php
-					} else if(isset($_GET['code']) && $_GET['code'] == 5){
-						?><p class="code"> Erfolgreich Ausgeschrieben</p> <?php
-					}
-					?>
+				if(isset($_GET['code']) && $_GET['code'] == 4){
+					?><p class="code"> Bitte füllen Sie alles aus </p> <?php
+				} else if(isset($_GET['code']) && $_GET['code'] == 5){
+					?><p class="code"> Erfolgreich Ausgeschrieben</p> <?php
+				}
+			?>
 
-					<form action="postCredit.php" method="post">
-						Kredithöhe <br/>
-						<input type="number" name="credit" size="20">
-						<br/>
-						Zins (%) <br/>
-						<input type="number" name="interest" size="20">
-						<br/>
-						<input type="submit" value="Auschreiben">
-						<input type="reset" value="Reset">
-					</form>
-			</div>
-			<div class="w50">
+				<form action="postCredit.php" method="post">
+					Kredithöhe <br/>
+					<input type="number" name="credit" size="20">
+					<br/>
+					Zins (%) <br/>
+					<input type="number" name="interest" size="20">
+					<br/>
+					<input type="submit" value="Auschreiben">
+					<input type="reset" value="Reset">
+				</form>
+
 				<?php
 				echo "<h4> Kreditannehmen </h4>";
 				if(isset($_GET['code']) && $_GET['code'] == 8){
@@ -127,8 +120,8 @@
 					?><p class="code"> Erfolgreich Angenommen</p> <?php
 				}
 				include "creditRequest.php";
-				}?>
-			</div>
+				}
+			?>
 		</div>
 		<div class="row">
 			<?php
